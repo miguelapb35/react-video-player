416afc67d3091a95f938a69870691413ef59039c
Miguel Angel <miguelapb35@gmail.com>
2020-04-16 21:07:38 -0500

    Updates

---
+++ precache-manifest.a7d965f65075afe3027278b98b0b62e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd39361aef29f757048528ab8816f1e0",
+    "url": "/react-video-player/index.html"
+  },
+  {
+    "revision": "de19f34740b5d7aa105f",
+    "url": "/react-video-player/static/js/2.04d12344.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-video-player/static/js/2.04d12344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2908aefdcc5e8b35c5d0",
+    "url": "/react-video-player/static/js/main.bd254ba9.chunk.js"
+  },
+  {
+    "revision": "0479a09a00d1315cbb64",
+    "url": "/react-video-player/static/js/runtime-main.dc6db76a.js"
+  }
+]);
